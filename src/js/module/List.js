5a15ea75331a0730b67a279db77073fa4db69581
Mo sohyun <shmo.bs.office@gmail.com>
2024-04-15 18:40:34 +0900

    [ing] 리스트 카테고리 기능 추가

--- src/js/module/List.js
+++ src/js/module/List.js
@@ -1,19 +1,26 @@
 export default class List {
-  constructor({ data, id, render }) {
-    this.data = data;
-    this.target = document.getElementById(id);
-    this.render = render;
+  constructor({ ...prop }) {
+    this.data = prop.data;
+    this.categoryData = this.data;
+    this.target = document.getElementById(prop.id);
+    this.renderHtml = prop.renderHtml;
+    this.category = !prop.category ? false : prop.category;
 
     this.init();
   }
 
-  init() {
+  printList() {
     let listHtml = '';
 
-    this.data.forEach((item) => {
-      listHtml += this.render(item);
+    this.categoryData.forEach((item) => {
+      listHtml += this.renderHtml(item);
     });
 
     this.target.innerHTML = listHtml;
+  }
+
+  init() {
+    console.log(this.category);
+    this.printList();
   }
 }
--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -3,13 +3,16 @@ import 'swiper/css';
 import dataList from '../data.json';
 import List from './module/List';
 
-// const portfolioList = new List({
-//   data: dataList.result,
-//   id: 'portfolio-list',
-//   render(data) {
-//     return `<li>${data.title} : ${data.date}</li>`;
-//   },
-// });
+const portfolioList = new List({
+  data: dataList.result,
+  id: 'portfolio-list',
+  renderHtml(data) {
+    return `<li>${data.title} : ${data.date}</li>`;
+  },
+  category: {
+    id: 'portfolioTab',
+  },
+});
 
 const tabSwiper = new Swiper('#portfolioTab', {
   slidesPerView: 'auto',
