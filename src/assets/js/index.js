2f7a20acad61c0c4e83b8a6c3e1d7389dc8e5085
Mo sohyun <shmo.bs.office@gmail.com>
2024-04-19 13:19:49 +0900

    [ing] 메인페이지 리스트 추가

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,16 +3,42 @@ import 'swiper/css/bundle';
 import dataList from './data.json';
 import List from './module/List';
 
-// const portfolioList = new List({
-//   data: dataList.result,
-//   id: 'portfolio-list',
-//   render(data) {
-//     return `<li>${data.title} : ${data.date}</li>`;
-//   },
-// });
+const portfolioList = new List({
+  data: dataList.result,
+  id: 'portfolioList',
+  render(data) {
+    const { category } = data;
+
+    return `<div class="swiper-slide portfolio-swiper__item">
+      <a href="/portfolio/detail/?id=${data.id}" class="img-box">
+        <img src="./assets/images/main_portfolio_swiper_${data.id}_pc.jpg" alt="" class="m-hide">
+        <img src="./assets/images/main_portfolio_swiper_${data.id}_mo.jpg" alt="" class="m-show">
+      </a>
+
+      <!-- 슬라이드 내 텍스트 -->
+      <div class="portfolio-swiper__txt">
+        <p class="portfolio-swiper__title fadeInUp--01">${data.title}</p>
+        <dl class="portfolio-swiper__desc en">
+          <div class="portfolio-swiper__cont fadeInUp--02">
+            <dt class="portfolio-swiper__info portfolio-swiper__info--typea">Date : </dt>
+            <dd class="portfolio-swiper__info">${data.date}</dd>
+          </div>
+          <div class="portfolio-swiper__cont fadeInUp--03">
+            <dt class="portfolio-swiper__info portfolio-swiper__info--typea">Brand : </dt>
+            <dd class="portfolio-swiper__info kr">${data.brand}</dd>
+          </div>
+          <div class="portfolio-swiper__cont fadeInUp--04">
+            <dt class="portfolio-swiper__info portfolio-swiper__info--typea">Type : </dt>
+            <dd class="portfolio-swiper__info">${category.replace(/^[a-z]/, (char) => char.toUpperCase())}</dd>
+          </div>
+        </dl>
+      </div>
+    </div>`;
+  },
+});
 
 // main 포트폴리오 영역 스와이퍼
-const swiper = new Swiper('#main-portfolio', {
+const swiper = new Swiper('#mainPortfolio', {
   slidesPerView: 'auto',
   navigation: {
     nextEl: '.swiper-button-next',
