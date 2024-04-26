0e5a16d9321407637d8bd3dec94417778d85bd4d
Mo sohyun <shmo.bs.office@gmail.com>
2024-04-26 15:48:30 +0900

    [end] portfolio 페이지 로드 처리 해제

--- src/assets/js/portfolio.js
+++ src/assets/js/portfolio.js
@@ -5,53 +5,51 @@ import StickyHeader from './module/StickyHeader';
 import List from './module/List';
 import Scroll from './module/Scroll';
 
-window.addEventListener('load', () => {
-  const header = new StickyHeader();
+const header = new StickyHeader();
 
-  const portfolioList = new List({
-    data: dataList.result,
-    id: 'portfolioList',
-    setImgUrl(id) {
-      const imgVer = {};
+const portfolioList = new List({
+  data: dataList.result,
+  id: 'portfolioList',
+  setImgUrl(id) {
+    const imgVer = {};
 
-      return {
-        pc: `/assets/images/portfolio_thumb_${id}_pc${imgVer[id]?.pc ? imgVer[id].pc : ''}.jpg`,
-        mo: `/assets/images/portfolio_thumb_${id}_mo${imgVer[id]?.mo ? imgVer[id].mo : ''}.jpg`,
-      };
-    },
-    renderHtml(data) {
-      return `<li class="list__item">
-        <a href="./detail/?id=${data.id}" class="list__link">
-          <div class="img-box">
-            <img src="${data.imgUrl.pc}" alt="" class="m-hide">
-            <img src="${data.imgUrl.mo}" alt="" class="m-show">
-          </div>
-          <div class="list__text">
-            <p class="list__title">${data.title}</p>
-            <p class="list__data">${data.date}_${data.brand}</p>
-          </div>
-        </a>
-      </li>`;
-    },
-    useCategory: {
-      id: 'portfolioTab',
-      default: 'all',
-    },
-    usePaging: {
-      id: 'btnMore',
-      increase: 6,
-    },
-  });
+    return {
+      pc: `/assets/images/portfolio_thumb_${id}_pc${imgVer[id]?.pc ? imgVer[id].pc : ''}.jpg`,
+      mo: `/assets/images/portfolio_thumb_${id}_mo${imgVer[id]?.mo ? imgVer[id].mo : ''}.jpg`,
+    };
+  },
+  renderHtml(data) {
+    return `<li class="list__item">
+      <a href="./detail/?id=${data.id}" class="list__link">
+        <div class="img-box">
+          <img src="${data.imgUrl.pc}" alt="" class="m-hide">
+          <img src="${data.imgUrl.mo}" alt="" class="m-show">
+        </div>
+        <div class="list__text">
+          <p class="list__title">${data.title}</p>
+          <p class="list__data">${data.date}_${data.brand}</p>
+        </div>
+      </a>
+    </li>`;
+  },
+  useCategory: {
+    id: 'portfolioTab',
+    default: 'all',
+  },
+  usePaging: {
+    id: 'btnMore',
+    increase: 6,
+  },
+});
 
-  const tabSwiper = new Swiper('#portfolioTab', {
-    slidesPerView: 'auto',
-  });
+const tabSwiper = new Swiper('#portfolioTab', {
+  slidesPerView: 'auto',
+});
 
-  const $scrollBox = document.querySelectorAll('.js-scroll');
+const $scrollBox = document.querySelectorAll('.js-scroll');
 
-  Array.from($scrollBox).forEach((item) => {
-    const scroll = new Scroll({
-      target: item,
-    });
+Array.from($scrollBox).forEach((item) => {
+  const scroll = new Scroll({
+    target: item,
   });
 });
